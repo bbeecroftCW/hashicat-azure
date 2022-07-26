d23f3d9bea8a8fc3fa2d91e84e5f6ecbb7905a76
bbeecroftCW <108306385+bbeecroftCW@users.noreply.github.com>
2022-07-26 11:38:16 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to BB's HashiCat Training App! Solving the world's problems one MEOW at a time :) 
   <!-- END -->
   
   </div>
